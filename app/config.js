b745a9d1996d96669988cccfd1b20fea0f302604
herozeng <1055670399@qq.com>
2018-04-01 14:24:14 +0800

    change back-end port

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ export default (() => {
   window.gconfig = {};
   +(function (global) {
     // 本地开发打开的路径以及端口
-    global.linkUrl = 'http://localhost:8088';
+    global.linkUrl = 'http://39.106.180.208:8088';
     // global.linkUrl = 'http://localhost:3030';
     if (process.env.NODE_ENV === 'production') { // 生产环境用不同的接口地址
       global.linkUrl = 'http://localhost:3000';
